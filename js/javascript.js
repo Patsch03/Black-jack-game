8e599ff5f82410c962b60d41de01fe2dbe1538c5
Patsch03 <pschenk0304@gmail.com>
2022-08-09 17:25:31 -0400

    fixed bug where dealer ace wasnt calculated correctly

--- js/javascript.js
+++ js/javascript.js
@@ -5,7 +5,7 @@ let dealerCards = 2;
 //CARD CLASS CREATS A CARD THAT HAS A RANDOM SUIT AND RANDOM VALUE 1-10
 class card{
     suit = "null";
-    value =  0;
+    value = 0;
     ace = 0;
 // MAKES A CONSTRUCTOR THAT ADDS THE RANDOM SUIT AND VALUES TO THE OBJECT.
     constructor(){
@@ -21,7 +21,7 @@ class card{
         }
 
         let valuePick = Math.random();
-        if(valuePick <= 0.07692307692){
+        if(valuePick <= 0.0692307692){
             this.value = 1; // ace needs implementation
         }else if(valuePick <= 0.15384615384){
             this.value = 2;
@@ -34,7 +34,7 @@ class card{
         }else if(valuePick <= 0.46153846152){
             this.value = 6;
         }else if(valuePick <= 0.53846153844){
-            this.value = 7;
+             this.value = 7;
         }else if(valuePick <= 0.61538461536){
             this.value = 8;
         }else if(valuePick <= 0.69230769228){
@@ -89,6 +89,9 @@ standBut.addEventListener('click', stand);
 const hitBut = document.getElementById('Hit');
 hitBut.addEventListener('click', hit);// checks for button being hit 
 
+const doubleBut = document.getElementById("Double");
+doubleBut.addEventListener('click', double);
+
 //ADDS CARD TO PLAYER HAND AND CONSOLE LOGS IT, CHECKS TOO SEE IF THE PLAYERHAND BUSTS OR IF PLAYERHAND = 21 AND REMOVES EVENTLISTENER ON BUTTONS
 function hit(){
     playerHand[handCards] = new card; // adds new card to player hand array
@@ -111,8 +114,15 @@ function buttonStop(){
     
     const standButR = document.getElementById("Stand");
     standButR.removeEventListener('click', stand);
+
+    const doubleButR = document.getElementById("Double");
+    doubleButR.removeEventListener('click', double);
+
+    
     
 }
+
+
 
 //STAND LOGIC, ALSO STARTS DEALER "AI"
 function stand(){
@@ -127,9 +137,53 @@ function stand(){
 
 //CHECKS TO SEE WHO WON
 function checkWin(){
+
+    if(dealerHandValue().toString().includes("/") && playerHandValue().toString().includes("/")){
+        let dealHand2 = dealerHandValue();
+        const dealHand2Val = dealHand2.substring(dealHand2.indexOf("/") + 2);
+        let compValD = parseInt(dealHand2Val);
+
+        let playHand2 = playerHandValue();
+        const playHand2Val = playHand2.substring(playHand2.indexOf("/") + 2);
+        let compValP = parseInt(playHand2Val);
+
+        if(compValP < 21 && compValP > compValD){
+            console.log("Player wins");
+        }else if(compValP < 21 && compValP < compValD){
+            console.log("Dealer wins");
+        }else if(compValP == compValD){
+            console.log("push");
+        }
+    }
+    if(dealerHandValue().toString().includes("/")){
+        let dealT = dealerHandValue();
+        const afterSl = dealT.substring(dealT.indexOf("/") + 2);
+        let dealAce = parseInt(afterSl);
+        console.log(dealAce);
+        if(dealAce <= 21 && dealAce > 16){
+            if(dealAce > playerHandValue()){
+                console.log("dealer wins");
+            }else if(dealAce < playerHandValue()){
+                console.log("player wins");
+            }else if(dealAce == playerHandValue()){
+                console.log("push");
+            }
+        }else{
+            if(dealerHandValue() > 21){
+                console.log("Dealer busts");
+            }else if(dealerHandValue() > playerHandValue()){
+                console.log("dealer wins");
+            }else if(dealerHandValue() < playerHandValue()){
+                console.log("player wins");
+            }else if(dealerHandValue() == playerHandValue()){
+                console.log("push");
+            }
+        }
+    }
+
     if(playerHandValue().toString().includes("/")){ 
         let thing = playerHandValue();
-        const afterS = thing.substring(thing.indexOf("/") + 2)
+        const afterS = thing.substring(thing.indexOf("/") + 2);
         let aceVal = parseInt(afterS);
         if(aceVal > 21){
             console.log("Player loses!")
@@ -162,7 +216,9 @@ function checkWin(){
 }
 
 function double(){
-
+    hit();
+    stand();
+    buttonStop();
 }
 
 //RETURNS THE VALUE OF THE PLAYERS CARDS
@@ -200,15 +256,49 @@ function playerHandValue(){
 //RETURNS THE VALUE OF THE DEALERS HAND
 function dealerHandValue(){
     let sum = 0;
+    let sumAce = 0;
+    let bool = false;
     for(let i = 0; i < dealerHand.length; i++){
-        sum += dealerHand[i].value;
+        if(dealerHand[i].value == 1){
+            sumAce += 11;
+            sum += 1;
+        }else{
+            sum += dealerHand[i].value;
+            sumAce += dealerHand[i].value;
+        }
+        
+    }
+//DEALER ACE IMPLEMENTATION
+    for(let i = 0; i < dealerHand.length; i++){
+        if(dealerHand[i].value == 1){
+            bool = true;
+        }
+    }
+    if(bool){
+        if(sumAce > 21){
+            return sum;
+        }
+        return sum + " / " + sumAce;
+    }else{
+        return sum;
     }
 
-    return sum;
 }
 
 //LOGIC FOR DEALER "AI"
 function dealerPlay(){
+    if(dealerHandValue().toString().includes("/")){
+        let fullRet = dealerHandValue();
+        let secNum = fullRet.substring(fullRet.indexOf("/") + 2); 
+        while(parseInt(secNum) <= 16){
+            dealerHand[dealerCards] = new card;
+            console.log(dealerHand[dealerCards].suit + " " + dealerHand[dealerCards].value);
+            dealerHandValue();
+            console.log("Dealer hand value " + dealerHandValue());
+            secNum = secNum + dealerHand[dealerCards].value;
+            dealerCards += 1;
+        }
+    }
     while(dealerHandValue() <= 16){
         dealerHand[dealerCards] = new card;
         console.log(dealerHand[dealerCards].suit + " " + dealerHand[dealerCards].value);
